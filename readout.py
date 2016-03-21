d4ab23f1fb23c277f5c003cc4e23b37d9ad29dbc
BBelonder <bernhard.bohlender@web.de>
2016-03-21 17:17:26 +0100

    Added files via upload

--- readout.py
+++ readout.py
@@ -19,7 +19,7 @@ def getIDs():
 	global port
 	VendorID=int(input("VendorID? Hex must start with 0x! Use lsusb in terminal to find your VID PID "),0) # hex geht so auch
 	ProductID=int(input("ProductID? Hex must start with 0x! "),0)
-	port=input("Which /dev/ is your usb to serial bridge on? Use 'dmesg | grep tty' to find it. Input must be in '/dev/ttyUSBX'")
+	port=input("Which /dev/ is your usb to serial bridge on? Use 'dmesg | grep tty' to find it. Input must have form '/dev/ttyUSBX' ")
 
 
 def startline(self):
@@ -43,39 +43,55 @@ def filename():
 	"""
 	i=datetime.datetime.now()
 	a="{0:02d}-{1:02d}-{2:02d}--{3:02d}.{4:02d}.txt".format(i.year,i.month,i.day,i.hour,i.minute)
-	print("Filename will be: "+a)
+#	print("Filename will be: "+a)
 	return a
 
 def set_folder():
 	"""
 	Defines the folder where the data will be stored
 	"""
-	folder=str(input("Where do you want to store your data? Folder must be given in str format. "))
+	folder=str(input("Where do you want to save your data? Folder must be given in str format. "))
 	global outp
-	outp=open(folder+filename(),"w")
-
-set_folder()	
-getIDs()
-Oszi=TDS2000b.TDS2000bDriver(VendorID,ProductID)
-Oszi.SetChannels(1,2)
-print(Oszi.readChannels())
-#print(Oszi.__dict__)
-#outp=open("/home/pi/LabData/"+filename(),"w")
-CH=Oszi.readChannels()
-print(CH)
-print(startline(Oszi))
-
-outp.write(startline(Oszi))
-max_time=int(input("Wie lange soll ich jetzt aufzeichnen? "))
-start_time=time.time()
-a=0
-while a <= max_time: #time.time()-start_time <= max_time:
-	a=time.time()-start_time
-	b=Oszi.ReadChanSingleValues()
-	print(b)
-	out_line="{0:8.3f};{1:9.6f};{2:9.6f}\n".format(a,b[0],b[1])
-	outp.write(out_line)
-	#time.sleep(0.3) #ohne die Begrenzung immerhin ne Messung alle 0.2s!
-	#a+=.2
+	print("Data will be saved to "+folder+"/"+filename())
+	outp=open(folder+"/"+filename(),"w")
 	
-outp.close()
+def write_line():
+	"""
+	writes line into file, yet fixed on device "Oszi", not done yet
+	"""
+	max_time=int(input("How long am I supposed to track data? (s)"))
+	start_time=time.time()
+	global current_time
+	current_time=0
+	while current_time <= max.time:
+		b=Oszi.ReadChanSingleValues()
+		out_line=[current_time]
+		for ch in Oszi.readChannels():
+			outline.append(Oszi)
+		current_time=time.time()-start_time
+	
+
+if __name__=="__main__":
+	set_folder()	
+	getIDs()
+	Oszi=TDS2000b.TDS2000bDriver(VendorID,ProductID)
+	Oszi.SetChannels(1,2)
+	Oszi.SetWidth(1)
+	print(Oszi.readChannels())
+	#print(Oszi.__dict__)
+	#outp=open("/home/pi/LabData/"+filename(),"w")
+	#print(startline(Oszi))
+
+	outp.write(startline(Oszi))
+	max_time=int(input("Wie lange soll ich jetzt aufzeichnen? "))
+	start_time=time.time()
+	a=0
+	while a <= max_time:
+		a=time.time()-start_time
+		b=Oszi.ReadChanSingleValues()
+		#print(b)
+		out_line="{0:8.3f};{1:9.6f};{2:9.6f}\n".format(a,b[0],b[1])
+		outp.write(out_line)
+		#time.sleep(0.3) #ohne die Begrenzung immerhin ne Messung alle 0.2s!
+	
+	outp.close()
