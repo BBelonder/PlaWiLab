9ddb0fe48f82c59bab4b80655933cca5f588b66f
BBelonder <bernhard.bohlender@web.de>
2016-03-21 17:17:48 +0100

    Added files via upload

--- drivers/TDS2000b.py
+++ drivers/TDS2000b.py
@@ -4,6 +4,7 @@
 #from __future__ import print_function
 import usbtmc
 import time
+import ast
 
 class TDS2000bDriver():
 	"""
@@ -29,6 +30,8 @@ class TDS2000bDriver():
 		Vendor ID and ProductID, in dec. hex values mus start with 0x.
 		Enc is signed Ascii by default. If you change that, you'll have 
 		to adjust self.__widthD yourself.
+		Work in Progress: The measurement methods are now being 
+		implemented, aim is to archieve better time resolution
 		"""
 		self.__VID=VendorID
 		self.__PID=ProductID
@@ -43,6 +46,100 @@ class TDS2000bDriver():
 		self.SetEncAscii()
 		self.__unitD={}
 		self.ReadUnit()
+		self.__Offset={}
+		self.Offset()
+		self.__Inv={}
+		self.ReadInv()
+		#self.SetImmedTypes("MEAN")
+		self.__IMMESrcD=dict.fromkeys(range(1,5))
+		self.__IMMETypD=dict.fromkeys(range(1,5))
+		self.__IMMEUniD=dict.fromkeys(range(1,5))
+		self.ReadImmedUnits()
+		self.__IMMEValD=dict.fromkeys(range(1,5))
+		#self.
+
+	def SetImmedTypes(self,Type):
+		"""
+		Sets Type of immed measurement. Type can be: FREQ, MEAN, PERI, 
+		PK2pk, MINI, MAXI, RIS, FALL, PWI, NWI, CRM. See TDS Programmer's
+		Manual p2-158ff.
+		"""
+		for n in self.__IMMETypeD:
+			self.write("measurement:immed:type "+str(Type))
+			self.__IMMETypeD[n]=str(Type)
+		return self.__IMMETypeD
+		
+	def SetImmedType(self,key,ch,Type):
+		"""
+		Sets the type for a specific key in the readout procedure
+		"""
+		self.__IMMETypD[key]=Type
+		while type(ch)!=int:
+			ch=ast.literal_eval(input("Please only give channel number! "))
+				
+		self.__IMMESrcD[key]=ch		
+		
+	def GetImmedValues(self):
+		"""
+		Grabs the immed values and puts them into dict
+		"""
+		self.__IMMEValD={}
+		for n in self.__IMMETypD:
+			self.write("measurement:immed:type "+str(self.__IMMETypD[n])+";Source "+str(self.__IMMESrcD[n]))
+			self.IMMEValD[n]=self.ask("measurement:immed:value?")
+		
+	def ReadImmedSrcs(self):
+		"""
+		Reads current Source for Immed measurement
+		"""
+		return self.__IMMESrcD
+		
+	def AddImmedSrc(self,key,Src):
+		"""
+		Adds/replaces Source for immed measurement		
+		"""
+		while Src not in [CH1,CH2,CH3,CH4,"CH1","CH2","CH3","CH4"]:
+			Src=ast.literal_eval(input("Please repeat your input in terms of 'CHx' :"))	
+		d={key:str(Src)}
+		self.__IMMESrcD.update(d)
+		return str(Src)+" has been added to readout routine. Key for this Channel is "+str(key)
+		
+	def AddImmedSrcs(self,keys,srcs):
+		"""
+		Adds/replaces several 
+		"""
+		while len(keys)!=len(srcs):
+			print("The number of keys does not match the number of sources!\n")
+			keys=ast.literal_eval(input("Please enter your keys again: "))
+			srcs=ast.literal_eval(input("Pleas enter your sources again: "))
+		for n in range(len(src)):
+			if src[n] in [CH1,CH2,CH3,CH4,"CH1","CH2","CH3","CH4"]:
+				pass
+			else:
+				print("The Channels must be given in Terms of 'CHx'\n")
+				src[n]=str(ast.literal_eval(input("Please repeat the Channel to key no."+str(key[n]))))		
+		self.__IMMESrcD=dict(zip(keys,srcs))
+		return self.__IMMESrcD
+		
+		
+	def ReadImmedUnits(self):
+		"""
+		Reads the Units from the immed measurement and puts them into
+		__IMMEUnitD. Returns the dict.
+		"""
+		for n in self.__IMMEUnitD:
+			self.__IMMEUnitD[n]=self.ask("measurment:immed:Units?")
+		return self.__IMMEUnitD
+		
+	def ReadImmedVals(self):
+		"""
+		Reads the immed Values and puts them into dict
+		"""
+		for n in self.__IMMESrcD:
+			self.write("measurement:immed:Source "+str(self.__IMMESrcD[n])+";Type "+str(self.__IMMETypD[n]))
+			self.__IMMEValD[n]=self.ask("measurement:immed:value?")
+		return self.__IMMEValdD	
+		
 		
 	def ReadUnit(self):
 		"""
@@ -68,7 +165,31 @@ class TDS2000bDriver():
 		for n in self.__Channels:
 			setting=self.ask("CH"+str(n)+"?")
 			self.__voltsDiv[n]=float(setting.split(";")[2]) #reads out volts/div Settings from oszi
+			
+	def Offset(self):
+		"""
+		Reads current Channel offsets
+		"""
+		self.__Offset=dict.fromkeys(self.__Channels)
+		for n in self.__Channels:
+			offset=self.ask("CH"+str(n)+"?")
+			self.__Offset[n]=float(offset.split(";")[3])#gives Offset in divs
 		
+	def ReadInv(self):
+		"""
+		looks up if the channels are inverted
+		"""
+		self.__Inv=dict.fromkeys(self.__Channels)
+		for n in self.__Channels:
+			inv=self.ask("CH"+str(n)+"?").split(";")[-2]
+			if inv== "ON":
+				self.__Inv[n]=-1
+			elif inv=="OFF":
+				self.__Inv[n]=1
+			else:
+				raise TypeError("Da stimmt was net mit den Invs")
+		return self.__Inv	
+			
 	def ask(self,query):
 		"""
 		Hands given query to the oszilloscope
@@ -133,7 +254,7 @@ class TDS2000bDriver():
 		"""
 		Reads current Endpoint for DataAquisiton
 		"""
-		return self.ask("Data:Start?")
+		return self.ask("Data:End?")
 		
 	def SetSource(self,Channel):
 		"""
@@ -172,7 +293,7 @@ class TDS2000bDriver():
 		val=[]
 		for n in self.__Channels:
 			self.SetSource(n)
-			val.append(int(self.ask("Curve?"))/self.__widthD[self.__width]*5*self.__voltsDiv[n])
+			val.append(float(self.__Inv[n])*(int(self.ask("Curve?"))+self.__Offset[n])/self.__widthD[self.__width]*5*self.__voltsDiv[n])
 			#die self.__withD-Konstruktion ist nötig, um die übertragungsbreiten bei der Bestimmung
 			#des Bereichs zu berücksichtigen
 		return val
@@ -180,12 +301,17 @@ class TDS2000bDriver():
 if __name__=="__main__":
 	start_time=time.time()
 	x=TDS2000bDriver(1689,869)#check
-	print(x.ask("*IDN?"))#check
+	setup_t=time.time()-start_time
+	print("Das Setup dauert "+str(setup_t)+"s.\n")
+	#print(x.ask("*IDN?"))#check
 	print(x.__dict__)#check
-	x.SetChannels(1,2)#check
-	print(x.readChannels())#check	
-	print(x.ReadChanSingleValues())
-	print(x.ReadUnit())
+	#x.SetChannels(1,2)#check
+	#print("\n Read Inv: ",x.ReadInv(),"\n")
+	#print(x.readChannels())#check	
+	#print(x.ReadChanSingleValues())
+	#print(x.ReadUnit())
+		
+	
 #	x.SetChannels(2,3,1)
 #	print(x.ReadChanSingleValues())
 #	x.SetChannels(3)#check
@@ -204,9 +330,8 @@ if __name__=="__main__":
 """
 To Do List:
 
-Invertierung berücksichtigen! is setting.split(";")[-2] (vgl. divVolts): ON/OFF
-
 Irgendwie schauen, dass man nicht aktive Kanäle (am Oszi) rauswirft, sonst
 kann man die der Channellist hinzufügen und das Programm schmiert ab
+
 
 """
