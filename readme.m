16998410dc3e7466747f042f1061832776d41b91
BBelonder <bernhard.bohlender@web.de>
2016-03-21 17:18:57 +0100

    Update readme.m

--- readme.m
+++ readme.m
@@ -2,4 +2,4 @@ This is but a humble attempt to creat a script, that reads Values from an Tektro
 
 It includes a driver for the XGS600, which was taken from github.com/CINF/PyExpLabSys
 
-The Driver for the TDS2004b is work in progess and by now specified to read only the central datapoint of each channel.
+The Driver for the TDS2004b is work in progess and by now specified to read only the central datapoint of each channel. Work is now concentrating on the usage of measurement:immed method for TDS2000-family. Should work faster than the older method. But still work in progress.
